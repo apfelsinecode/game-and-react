89a3758d2c70557107799756a9ed69759802d098
apfelsinecode <apfelsinecode@users.noreply.github.com>
2022-09-05 22:07:43 +0200

    Game Board as table

---
+++ game-and-react/notes.md
@@ -0,0 +1,3 @@
+
+
+- index.html verwenden um Titel zu setzen
---
+++ game-and-react/src/Chef.tsx
@@ -0,0 +1,58 @@
+
+/*interface ChefProps {
+
+}*/
+
+/*
+ * use emojis:
+ * the four foods: 🥩, 🥦, 🥓, 🐟
+ * cook/chef: 🧑‍🍳
+ * pan: 🍳 (not perfect choice)
+ */
+
+function Chef(/*props: ChefProps*/) {
+    
+    const food = ['🥩', '🥦', '🥓', '🐟']; // food for the pans (wdefines idth)
+    const height = 4; // how high the food can be thrown (height above pan)
+
+    return (
+        <div className="Chef">
+            <h1>Game&amp;React: Chef</h1>
+            
+            <table className="GameTable">
+                <tbody>
+                    {Array.from(Array(height).keys()).map(i => (
+                        <tr key={i} className="Air">
+                            {food.map((elem, index) => (
+                                <td key={index}>{elem}</td>
+                            ))}
+                        </tr>
+                    ))}
+                    <tr>
+                        {food.map((elem, index) => (
+                            <td>🍳</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {food.map((elem, index) => (
+                            <td key={index}>{elem}</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {food.map((elem, index) => (
+                            <td>🍳</td>
+                        ))}
+                    </tr>
+                    <tr>
+                        {food.map((elem, index) => (
+                            <td key={index}>{elem}</td>
+                        ))}
+                    </tr>
+                </tbody>
+                
+            </table>
+        </div>
+    )
+}
+
+export default Chef;
