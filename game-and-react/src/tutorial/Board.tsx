7be7473db292ff7749eb239609123d8f1fca9e6c
apfelsinecode <apfelsinecode@users.noreply.github.com>
2022-12-27 18:03:19 +0100

    interactive again

--- game-and-react/src/tutorial/Board.tsx
+++ game-and-react/src/tutorial/Board.tsx
@@ -5,36 +5,41 @@ export default function Board() {
 
     const [values, setValues] = useState(Array(9).fill(null))
 
+    const handleClick = (i: number) => {
+        const valuesCopy = values.slice();
+        valuesCopy[i] = 'X';
+        setValues(valuesCopy);
+    }
+
     return (
         <>
             <div className="board-row">
-                <Square value={values[0]}/>
-                <Square value={values[1]}/>
-                <Square value={values[2]}/>
+                <Square value={values[0]} onClick={() => handleClick(0)}/>
+                <Square value={values[1]} onClick={() => handleClick(1)}/>
+                <Square value={values[2]} onClick={() => handleClick(2)}/>
             </div>
             <div className="board-row">
-                <Square value={values[3]}/>
-                <Square value={values[4]}/>
-                <Square value={values[5]}/>
+                <Square value={values[3]} onClick={() => handleClick(3)}/>
+                <Square value={values[4]} onClick={() => handleClick(4)}/>
+                <Square value={values[5]} onClick={() => handleClick(5)}/>
             </div>
             <div className="board-row">
-                <Square value={values[6]}/>
-                <Square value={values[7]}/>
-                <Square value={values[8]}/>
+                <Square value={values[6]} onClick={() => handleClick(6)}/>
+                <Square value={values[7]} onClick={() => handleClick(7)}/>
+                <Square value={values[8]} onClick={() => handleClick(8)}/>
             </div>
         </>
     );
 }
 
 interface SquareProps{
-    value: "X" | "O" | null
+    value: "X" | "O" | null;
+    onClick?: () => void;
 }
 
 function Square(props: SquareProps) {
 
-    /*const [value, setValue] = useState("");
-
-    const click = () => {
+    /*const click = () => {
         switch (value) {
             case "":
                 setValue("X");
@@ -49,6 +54,6 @@ function Square(props: SquareProps) {
         }
     }*/
 
-    return <button className="Square">{props.value}</button>
+    return <button onClick={props.onClick} className="Square">{props.value}</button>
 
 }
