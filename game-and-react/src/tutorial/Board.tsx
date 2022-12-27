4fbe6a390dc56d092e0f30b802add60a73280800
apfelsinecode <apfelsinecode@users.noreply.github.com>
2022-12-27 17:27:12 +0100

    9 squares

--- game-and-react/src/tutorial/Board.tsx
+++ game-and-react/src/tutorial/Board.tsx
@@ -1,6 +1,32 @@
-import Square from "./Square";
-
+import './styles.css'
 
 export default function Board() {
-    return <Square/>;
+    return (
+        <>
+            <div className="board-row">
+                <Square value='X'/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+            <div className="board-row">
+                <Square/>
+                <Square/>
+                <Square/>
+            </div>
+        </>
+    );
+}
+
+interface SquareProps {
+    value?: 'X' | 'O'
+}
+
+function Square(props: SquareProps) {
+    return <button className="Square">{props.value}</button>
+
 }
